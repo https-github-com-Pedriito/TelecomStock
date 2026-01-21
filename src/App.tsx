60170490f4db7e891f964cd87d8a5ba94a1fef2a
Pierre-Elie <promer@decimale.net>
2026-01-21 14:44:27 +0100

    analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import { AdminPortal } from './pages/AdminPortal';
 import { FeedbackProvider } from './components/UXFeedback';
 import { MobileBottomNav } from './components/MobileBottomNav';
 import { UserProfileModal } from './components/UserProfileModal';
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const [currentView, setCurrentView] = useState<ViewMode>('dashboard');
